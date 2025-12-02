de1bc99c34221fbb212f13df9753baf7a6910d5c
(major) john (major) <iamh2o@gmail.com>
2025-12-02 00:11:27 -0800

    Rewrite repository as bash scripts

--- src/boto3/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A lightweight subset of the :mod:`boto3` package used for testing."""
-
-from . import session
-from .session import Session
-
-__all__ = ["Session", "session"]
--- src/botocore/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Minimal subset of the :mod:`botocore` package for the test suite."""
-
-from . import exceptions, response, stub
-
-__all__ = ["exceptions", "response", "stub"]
